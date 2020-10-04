f012462a42b40ff751afb17ef4a9f51b934db372
Alcides A <aaugusto.kd@gmail.com>
2020-10-04 14:49:35 -0300

    detach routes from react-router-dom components

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "../components/Home";
-import Project from "../pages/Project";
-import ProjectList from "../pages/ProjectList";
+import routes from './routes'
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/projects" component={ProjectList}></Route>
-        <Route path="/project/:project_id" component={Project}></Route>
+        {
+          routes.map((route, i) => <Route key={i} {...route}/>)  
+        }
       </Switch>
     </Router>
   );
