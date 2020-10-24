dd2fa0322b1f090a765a0a6065043d052454e3b1
Pranjal <pranjaljain0697@gmail.com>
2020-10-24 14:08:30 +0530

    Minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,11 @@ import Navigation from "./Navigation";
 import homecards from "../data/homecards.json";
 import HomeCard from "./HomeCard";
 import Left from "./Left";
+import loadable from "@loadable/component";
+
+const HomeAbout = loadable(() => import("./HomeAbout"));
 
 export default function Home({ name }) {
-  // const [HomeCards, setHomeCards] = useState(
-
   const HomeCards = homecards.data;
 
   return (
@@ -21,18 +22,7 @@ export default function Home({ name }) {
               <Row className="RightUpperSec">
                 <Col className="RightUpperSecContent">
                   <h2>About Me</h2>
-                  <p>
-                    I discovered my passion for computer science when I was a
-                    sophomore in High School; it was then, when I started
-                    learning more for my passion. I am pursuing my bachelor's
-                    degree at Avantika University, major in Computer science and
-                    engineering. A year later I was introduced to Embedded
-                    systems, where I discovered the love for electrics and
-                    electronics and also for embedded systems. Then I got a
-                    scholarship opportunity to do a research work on Smart grid
-                    and started the never-ending journey of becoming a developer
-                    along with sharpening my eye for innovations.
-                  </p>
+                  <HomeAbout />
                 </Col>
               </Row>
               <Row className="RightLowerSec">
---
+++ src/components/HomeAbout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home_data from "../data/home_data.json";
+
+function HomeAbout() {
+    return (
+        <div>
+            <p>
+                {Home_data.data.about}
+            </p>
+        </div>
+    )
+}
+
+export default HomeAbout
