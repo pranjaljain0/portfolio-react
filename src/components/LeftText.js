1d544e542b1c6fdd8fa466ee32e60cf7764d45fe
jainpranjal0 <pranjal.j@collegepass.org>
2021-05-04 13:56:44 +0530

    :art: Prettier

--- src/components/LeftText.js
+++ src/components/LeftText.js
@@ -10,12 +10,7 @@ function LeftText() {
         <h1 className="name_big">Pranjal Jain</h1>
         <p className="animated_text">
           <Typed
-            strings={[
-              "Test 4",
-              "Test 3",
-              "Test 2",
-              "Test 1",
-            ]}
+            strings={["Test 4", "Test 3", "Test 2", "Test 1"]}
             typeSpeed={40}
             backSpeed={50}
             loop
