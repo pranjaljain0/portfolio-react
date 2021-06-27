2a2ba4cbdd26d34cc225c6d38a7a470646f12335
jainpranjal0 <pranjal.j@collegepass.org>
2021-06-27 18:01:05 +0530

    Added name to api

--- api/name.js
+++ api/name.js
@@ -1,7 +1,4 @@
 module.exports = (req, res) => {
-    res.json({
-        body: req.body,
-        query: req.query,
-        cookies: req.cookies,
-    })
+    const { name = 'World' } = req.query
+    res.status(200).send(`Hello ${name}!`)
 }
