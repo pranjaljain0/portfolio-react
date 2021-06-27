6fa6cd999c7b3851ca280ff5c8adb4debe68dd34
jainpranjal0 <pranjal.j@collegepass.org>
2021-06-27 17:45:51 +0530

    Updated serverless

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,7 @@
 module.exports = (req, res) => {
-    const { name = 'World' } = req.query
-    res.status(200).send(`Hello ${name}!`)
+    const {
+        query: { name },
+    } = req
+
+    res.send(`Hello ${name}!`)
 }
