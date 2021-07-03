ef45fa87efe2dd55e972c4431938235a77121d51
jainpranjal0 <pranjal.j@collegepass.org>
2021-07-04 03:31:56 +0530

    Added tracker

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,23 +1,18 @@
-import * as ackeeTracker from 'ackee-tracker'
-
 import React, { useEffect } from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import routes from './routes'
+import useAckee from 'use-ackee'
 
 export default function Routes() {
 
-	useEffect(() => {
-		const instance = ackeeTracker.create({
-			server: 'http://stats.pranjaljain.me/',
-			domainId: 'e04eb40f-8f6d-4cca-8c46-7a0df821c53d'
-		}, {
-			ignoreLocalhost: true,
-			// doNotTrack: false
-		})
-		const attributes = ackeeTracker.attributes()
-		instance.record(attributes)
-	}, [])
+	useAckee('/', {
+		server: 'https://stats.pranjaljain.me',
+		domainId: 'e04eb40f-8f6d-4cca-8c46-7a0df821c53d'
+	}, {
+		detailed: true,
+		ignoreLocalhost: true,
+	})
 
 	return (
 		<Router>
