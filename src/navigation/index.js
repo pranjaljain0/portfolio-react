d6b733ada23b066b8e5cf57c19294850876a132b
jainpranjal0 <pranjal.j@collegepass.org>
2021-07-06 00:16:40 +0530

    Added sentry

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,10 +1,22 @@
+import * as Sentry from "@sentry/react";
+
 import React, { useEffect } from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
+import { Integrations } from "@sentry/tracing";
 import routes from './routes'
 import useAckee from 'use-ackee'
 
 export default function Routes() {
+	Sentry.init({
+		dsn: "https://aa99498b2e0149d4a2ac0dbf5d8ac712@o912654.ingest.sentry.io/5849881",
+		integrations: [new Integrations.BrowserTracing()],
+
+		// Set tracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production
+		tracesSampleRate: 1.0,
+	});
 
 	useAckee('/', {
 		server: 'https://stats.pranjaljain.me',
